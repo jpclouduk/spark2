f28c282237484034858f46b4e47f40cf942da1b9
jpclouduk <poulsenjx@gmail.com>
2018-09-06 20:56:30 +0100

    Update centos_install_spark2.sh

--- centos_install_spark2.sh
+++ centos_install_spark2.sh
@@ -1,42 +1,50 @@
 #!/bin/sh
 
 ##Author: jpclouduk
-##Rev: 1.2
-##Date: 05/08/2018
- 
+##Rev: 1.4
+##Date: 06/09/2018
+
+
+contin() {
+read -p "Do you want to exit? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    exit 0
+fi
+}
 
 # Update yum repos
-yum clean all
-yum -y install epel-release
-yum -y update
+sudo yum clean all
+sudo yum -y install epel-release
+sudo yum -y update
+contin
 
 # Install java and check
-yum install java -y
+sudo yum install java -y
 java -version
+contin
 
 # Install Scala
 wget http://www.scala-lang.org/files/archive/scala-2.10.1.tgz
-tar xvf scala-2.10.1.tgz
-sudo mv scala-2.10.1 /usr/lib
+sudo tar xf scala-2.10.1.tgz -C /usr/lib/
 sudo ln -s /usr/lib/scala-2.10.1 /usr/lib/scala
 export PATH=$PATH:/usr/lib/scala/bin
-scala -version
+contin
 
 # Install Spark
 wget http://www-eu.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
-tar -xzf spark-2.2.1-bin-hadoop2.7.tgz
-export SPARK_HOME=$HOME/spark-2.2.1-bin-hadoop2.7
+tar xzf spark-2.3.1-bin-hadoop2.7.tgz -C $HOME/
+export SPARK_HOME=$HOME/spark-2.3.1-bin-hadoop2.7
 export PATH=$PATH:$SPARK_HOME/bin
+contin
 
 # Update global env variables
-echo 'export PATH=$PATH:/usr/lib/scala/bin' >> .bash_profile
-echo 'export SPARK_HOME=$HOME/spark-1.6.0-bin-hadoop2.6' >> .bash_profile
-echo 'export PATH=$PATH:$SPARK_HOME/bin' >> .bash_profile
+sudo sh -c 'printf "\nexport PATH=$PATH:/usr/lib/scala/bin \nexport SPARK_HOME=$HOME/spark-2.3.1-bin-hadoop2.7 \nexport PATH=$PATH:$SPARK_HOME/bin" >> /etc/profile'
+contin
 
 # Update firewall
-firewall-cmd --permanent --zone=public --add-port=6066/tcp
-firewall-cmd --permanent --zone=public --add-port=7077/tcp
-firewall-cmd --permanent --zone=public --add-port=8080-8081/tcp
-firewall-cmd --reload
-
-
+sudo firewall-cmd --permanent --zone=public --add-port=6066/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=7077/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=8080-8081/tcp
+sudo firewall-cmd --reload
+contin
